2c5a7b651dd125d1b8feca240c28a872b3e5deb7
iperezx <i3perez@sdsc.edu>
2020-09-23 16:47:26 -0700

    Add client config and setup script

---
+++ client/setupClient.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get -y update
+sudo apt-get -y full-upgrade
+sudo apt-get -y install rpi-eeprom
+cd /lib/firmware/raspberrypi/bootloader/beta/
+#search for the newest bin pieeprom
+export pieepromFile=$(ls -Art | tail -n 1)
+sudo cp $pieepromFile new-pieeprom.bin
+sudo cp /home/pi/client/bootconf-pxeboot.txt bootconf.txt
+sudo rpi-eeprom-config --out netboot-pieeprom.bin --config bootconf.txt new-pieeprom.bin
+sudo rpi-eeprom-update -d -f ./netboot-pieeprom.bin
+sudo systemctl mask rpi-eeprom-update
