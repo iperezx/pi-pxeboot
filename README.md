4bf1f58dd89c62326cc7d6af8636e54c5087dbd1
iperezx <i3perez@sdsc.edu>
2020-09-23 17:18:14 -0700

    Add server configs and run script

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # pi-pxeboot
+Raspberry Pi PXE boot configuation for both client and server.
+
+## Client
+```bash
+bash client/setupClient.sh
+sudo reboot
+```
+
+
+## Server
+```bash
+bash server/setupServer.sh
+sudo reboot
+```
---
+++ server/setupServer.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#Installs and Upgrades
+sudo apt-get -y update
+sudo apt-get -y full-upgrade
+sudo apt-get -y install rpi-eeprom
+sudo apt-get -y install rsync dnsmasq nfs-kernel-server
+
+sudo mkdir -p /nfs/client1
+sudo -p /tftpboot
+sudo chmod 777 /tftpboot
+sudo rsync -xa --progress --exclude /nfs/client1 \
+    --exclude /etc/systemd/network/10-eth0.netdev \
+    --exclude /etc/systemd/network/11-eth0.network \
+    --exclude /etc/dnsmasq.conf \
+    / /nfs/client1
+
+cd /nfs/client1
+sudo mount --bind /dev dev
+sudo mount --bind /sys sys
+sudo mount --bind /proc proc
+sudo chroot . rm /etc/ssh/ssh_host_*
+sudo chroot . dpkg-reconfigure openssh-server
+sudo chroot . systemctl enable ssh
+sudo umount dev sys proc
+
+sudo cp 10-eth0.netdev /etc/systemd/network/10-eth0.netdev
+sudo cp 11-eth0.network /etc/systemd/network/11-eth0.network
+
+sudo systemctl stop dhcpcd
+sudo systemctl disable dhcpcd
+sudo cp dnsmasq.conf /etc/dnsmasq.conf
+
+sudo cp -r /boot/* /tftpboot/
+sudo systemctl enable systemd-networkd
+sudo systemctl enable dnsmasq.service
+sudo systemctl restart dnsmasq.service
+
+sudo cp cmdline.txt /tftpboot/cmdline.txt
+sudo cp exports /etc/exports
+sudo cp fstab /nfs/client1/etc/fstab
+
+sudo systemctl enable rpcbind
+sudo systemctl restart rpcbind
+sudo systemctl enable nfs-kernel-server
+sudo systemctl restart nfs-kernel-server
