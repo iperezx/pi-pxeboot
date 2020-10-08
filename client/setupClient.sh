2b5a67ff993e11e2271ce7a927f5e33418fa1efa
iperezx <i3perez@sdsc.edu>
2020-10-08 10:32:13 -0700

    Add path

--- client/setupClient.sh
+++ client/setupClient.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#base directory
+baseDir=/home/pi/pi-pxeboot/client/
 sudo apt-get -y update
 sudo apt-get -y full-upgrade
 sudo apt-get -y install rpi-eeprom
@@ -6,7 +8,7 @@ cd /lib/firmware/raspberrypi/bootloader/beta/
 #search for the newest bin pieeprom
 export pieepromFile=$(ls -Art | tail -n 1)
 sudo cp $pieepromFile new-pieeprom.bin
-sudo cp /home/pi/client/bootconf-pxeboot.txt bootconf.txt
+sudo cp ${baseDir}bootconf-pxeboot.txt bootconf.txt
 sudo rpi-eeprom-config --out netboot-pieeprom.bin --config bootconf.txt new-pieeprom.bin
 sudo rpi-eeprom-update -d -f ./netboot-pieeprom.bin
 sudo systemctl mask rpi-eeprom-update
